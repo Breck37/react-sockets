e7f9218a091d02372654fe5b6f306935edde2b78
B-eck37 <brent.eckert7@gmail.com>
2018-03-29 11:28:46 -0700

    ReadMe

--- server/socketServer.js
+++ server/socketServer.js
@@ -1,6 +1,8 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
       cors = require('cors'),
+
+      //Specific dependency for Socket.IO that integrates the dependency with our ExpressJS Server
       socket = require('socket.io');
 
 const app = express();
@@ -8,5 +10,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+
 const port = 3700;
-app.listen(port, () => console.log(`We be jamming to the tunes of ${port}`));
+
+//Create an 'instance of socket.io by passing in the 'Server'
+//This creates the opportunity to continue our normal server communications but also add socket communications
+const io = socket(app.listen(port, () => console.log(`We be jamming to the tunes of ${port}`)));
+
+app.get('/', (req, res) => {
+      res.send('<h1>Hello World</h1>')
+})
